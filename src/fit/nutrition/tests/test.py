49288009654643c68e0f964b1c69deff9177737c
apandy02 <aryaman.pandya@tufts.edu>
2024-12-29 00:08:58 -0500

    Introduced unittests

---
+++ src/fit/nutrition/tests/test.py
@@ -0,0 +1,5 @@
+from fit.nutrition.assistants import NutritionLogger
+
+logger = NutritionLogger()
+
+logger.natural_language_macros("I ate a salad with 100g of spinach, 100g of tomato, and 100g of cucumber.")
